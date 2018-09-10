0fd42b8e5f3bdcbb9719a536ec53250df392197d
Rob Goelz <RobGoelz@gmail.com>
2018-09-10 10:50:40 -0400

    feat: update AddressBook.js with methods to pass spec

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,3 +1,11 @@
 function AddressBook () {
+  const contacts = [];
 
+  this.addContact = function (contact) {
+    contacts.push(contact);
+  };
+
+  this.getContact = function (contact) {
+    return contacts[contact];
+  };
 }
