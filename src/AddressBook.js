62677bdea88b0a6839d860890bb769198d80f629
Rob Goelz <RobGoelz@gmail.com>
2018-09-10 10:52:34 -0400

    feat: update AddressBook.js with alternative solution to pass spec

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,11 +1,11 @@
 function AddressBook () {
-  const contacts = [];
-
-  this.addContact = function (contact) {
-    contacts.push(contact);
-  };
-
-  this.getContact = function (contact) {
-    return contacts[contact];
-  };
+  this.contacts = [];
 }
+
+AddressBook.prototype.addContact = function (contact) {
+  this.contacts.push(contact);
+};
+
+AddressBook.prototype.getContact = function (contact) {
+  return this.contacts[contact];
+};
